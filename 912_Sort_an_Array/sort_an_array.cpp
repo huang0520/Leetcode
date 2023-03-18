fcfd50a1ea452bfcf6d6e336554f28de1385d3a9
huang0520 <huangmorris4x@gmail.com>
2023-03-18 22:17:10 +0800

    sort_an_array Easy_understand

--- 912_Sort_an_Array/sort_an_array.cpp
+++ 912_Sort_an_Array/sort_an_array.cpp
@@ -5,81 +5,47 @@ using namespace std;
 
 class Solution {
    public:
-    vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums.begin(), nums.end() - 1, nums.size());
-        return nums;
-    }
+    vector<int> sortArray(vector<int> nums) {
+        if (nums.size() == 1) {
+            return nums;
+        }
 
-    auto split(vector<int>::iterator head, int size) -> vector<int>::iterator {
-        if (size == 1) return head;
-        return head + size;
+        // Split
+        auto mid = nums.begin() + nums.size() / 2;
+
+        auto left = sortArray(vector<int>(nums.begin(), mid));
+        auto right = sortArray(vector<int>(mid, nums.end()));
+
+        // Merge
+        return merge(left, right);
     }
 
    private:
-    auto mergeSort(vector<int>::iterator begin, vector<int>::iterator end,
-                   int size) -> void {
-        if (size == 1) {
-            return;
-        }
-        // Split
-        int left_size = size / 2;
-        auto left_begin = begin;
-        auto left_end = left_begin + left_size - 1;
-
-        // Left half
-        mergeSort(left_begin, left_end, left_size);
-
-        // Right half
-        int right_size = size - left_size;
-        auto right_begin = begin + left_size;
-        auto right_end = end;
-
-        mergeSort(right_begin, right_end, right_size);
-
-        // Merge
+    auto merge(vector<int> left, vector<int> right) -> vector<int> {
         vector<int> tmp;
-        tmp.reserve(size);
+        tmp.reserve(left.size() + right.size());
 
-        // Choose smaller one between first element of left part and right
-        // part
-        while (left_begin <= left_end && right_begin <= right_end) {
-            if (*left_begin < *right_begin) {
-                tmp.emplace_back(*left_begin);
-                left_begin++;
+        // Compare first value of two vector
+        auto l_itr = left.begin(), r_itr = right.begin();
+        while (l_itr != left.end() && r_itr != right.end()) {
+            if (*l_itr > *r_itr) {
+                tmp.emplace_back(*r_itr);
+                r_itr++;
             } else {
-                tmp.emplace_back(*right_begin);
-                right_begin++;
-            }
-        };
-
-        // If left part all store in tmp first
-        if (left_begin > left_end) {
-            for (; right_begin <= right_end; right_begin++) {
-                tmp.emplace_back(*right_begin);
-            }
-            // If right part all store in tmp first
-        } else {
-            for (; left_begin <= left_end; left_begin++) {
-                tmp.emplace_back(*left_begin);
+                tmp.emplace_back(*l_itr);
+                l_itr++;
             }
         }
 
-        for (auto var : tmp) {
-            *begin = var;
-            begin++;
+        for (; l_itr != left.end(); l_itr++) {
+            tmp.emplace_back(*l_itr);
         }
 
-        // cout << "tmp:\n";
-        // for (auto var : tmp) {
-        //     cout << var << ' ';
-        // }
-        // cout << "\n";
+        for (; r_itr != right.end(); r_itr++) {
+            tmp.emplace_back(*r_itr);
+        }
 
-        // do {
-        //     cout << *begin << " ";
-        //     begin++;
-        // } while (begin <= end);
-        // cout << "\n=====\n";
+        return tmp;
     }
 };
 
